140c27e7451ed55067ad30f74f7a72c4a43922a7
tienvv1234 <tienvv1234@gmail.com>
2023-02-14 23:04:55 +0700

    Using custom interceptor for serialization

--- mycv/src/interceptors/serialize.interceptor.ts
+++ mycv/src/interceptors/serialize.interceptor.ts
@@ -7,6 +7,7 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
+import { UserDto } from '../users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
     intercept(
@@ -14,16 +15,17 @@ export class SerializeInterceptor implements NestInterceptor {
         next: CallHandler<any>
     ): Observable<any> | Promise<Observable<any>> {
         // Run something before a request is handled by the request handler
-        console.log('Im running before the handler', context);
-
+        // console.log('Im running before the handler', context);
         return next.handle().pipe(
             map((data: any) => {
                 // Run something before the response is sent out
-                console.log('Im running after the handler', data);
+                // console.log('Im running after the handler', data);
 
-                // return plainToClass(this.serializer, data, {
-                //     excludeExtraneousValues: true,
-                // });
+                // this is the class that we want to transform the data into Json
+                // Expose in Class UserDto are used to control the data that is sent back to the client
+                return plainToClass(UserDto, data, {
+                    excludeExtraneousValues: true, // this will remove the properties that are not defined in the UserDto class
+                });
             })
         );
     }
