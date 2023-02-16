c911cdd0c3bc10106f69750ac9c5c01e40aaaae0
tienvv1234 <tienvv1234@gmail.com>
2023-02-16 10:26:13 +0700

    signin, signup apisand provider AuthService

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -14,18 +14,26 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-    constructor(private usersService: UsersService) {}
+    constructor(
+        private usersService: UsersService,
+        private authService: AuthService
+    ) {}
 
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
-        this.usersService.create(body.email, body.password);
+        return this.authService.signup(body.email, body.password);
     }
 
-    // @UseInterceptors(new SerializeInterceptor(UserDto))
-    @Get('/:id')
+    @Post('/signin')
+    signin(@Body() body: CreateUserDto) {
+        return this.authService.signin(body.email, body.password);
+    }
+
+    // @UseInterceptors(new SerializeInterceptor(UserDto('/:id')
     async findUser(@Param('id') id: string) {
         const user = await this.usersService.findOne(parseInt(id));
         if (!user) {
--- mycv/src/users/users.module.ts
+++ mycv/src/users/users.module.ts
@@ -3,10 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-
+import { AuthService } from './auth.service';
 @Module({
     imports: [TypeOrmModule.forFeature([User])], // this line will create a repository for the User entity
     controllers: [UsersController],
-    providers: [UsersService],
+    providers: [UsersService, AuthService],
 })
 export class UsersModule {}
