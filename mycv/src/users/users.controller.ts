5d922c4d8135afe0a7b5f4fe273c8d6d7e17adaf
tienvv1234 <tienvv1234@gmail.com>
2023-02-16 11:26:05 +0700

    session not set when identical cookie is sent

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -24,25 +24,32 @@ export class UsersController {
         private authService: AuthService
     ) {}
 
-    @Get('/colors/:color')
-    setColor(@Param('color') color: string, @Session() session: any) {
-        session.color = color;
-        return { message: 'color set' };
-    }
+    // @Get('/colors/:color')
+    // setColor(@Param('color') color: string, @Session() session: any) {
+    //     session.color = color;
+    //     return { message: 'color set' };
+    // }
 
-    @Get('/colors')
-    getColor(@Session() session: any) {
-        return session.color;
-    }
+    // @Get('/colors')
+    // getColor(@Session() session: any) {
+    //     return session.color;
+    // }
 
     @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
-        return this.authService.signup(body.email, body.password);
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
 
+    // cookie-session will set a cookie on the browser
+    // if the value is identical to the previous value then it will not send the cookie
+    // if the value is different then it will send the cookie
     @Post('/signin')
-    signin(@Body() body: CreateUserDto) {
-        return this.authService.signin(body.email, body.password);
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
 
     // @UseInterceptors(new SerializeInterceptor(UserDto('/:id')
