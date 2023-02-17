50168521707d7a60ffe262459c63aa451ce1697a
tienvv1234 <tienvv1234@gmail.com>
2023-02-17 15:21:03 +0700

    ConfigModule and ConfigService

--- mycv/src/app.module.ts
+++ mycv/src/app.module.ts
@@ -7,20 +7,30 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Report } from './reports/reports.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true, // this makes the config service available to all modules
             envFilePath: `.env.${process.env.NODE_ENV}`,
         }),
-        TypeOrmModule.forRoot({
-            // set up the database connection,
-            type: 'sqlite', // the method forRoot() is a shortcut for the forRootAsync() method
-            database: 'db.sqlite',
-            entities: [User, Report],
-            synchronize: true,
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'sqlite',
+                database: configService.get<string>('DB_NAME'),
+                synchronize: true,
+                entities: [User, Report],
+            }),
         }),
-
+        // TypeOrmModule.forRoot({
+        //     // set up the database connection,
+        //     type: 'sqlite', // the method forRoot() is a shortcut for the forRootAsync() method
+        //     database: 'db.sqlite',
+        //     entities: [User, Report],
+        //     synchronize: true,
+        // }),
         UsersModule,
         ReportsModule,
     ],
