0eafbaedd99d0e220497db73a03b2bb1b2792638
tienvv1234 <tienvv1234@gmail.com>
2023-02-17 10:34:31 +0700

    configModel and configService

--- mycv/src/app.module.ts
+++ mycv/src/app.module.ts
@@ -6,9 +6,13 @@ import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Report } from './reports/reports.entity';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true, // this makes the config service available to all modules
+            envFilePath: `.env.${process.env.NODE_ENV}`,
+        }),
         TypeOrmModule.forRoot({
             // set up the database connection,
             type: 'sqlite', // the method forRoot() is a shortcut for the forRootAsync() method
@@ -16,6 +20,7 @@ import { Report } from './reports/reports.entity';
             entities: [User, Report],
             synchronize: true,
         }),
+
         UsersModule,
         ReportsModule,
     ],
